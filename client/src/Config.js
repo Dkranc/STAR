dd5484650cb05a95eea36ecdc0fe40dbf42cec51
Dave <dvdkranc22@gmail.com>
2023-01-26 09:16:43 +0200

    added role to session storage

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,10 +1,10 @@
 
 /*still need to add this application to AZURE AD in order to authenticate*/
 const Config = {
-  appId: "app id from azure",
-  redirectUri: "http://localhost:3000",
-  scopes: ["user.read"],
-  authority: "https://login.microsoftonline.com/tennent.onmicrosoft.com",
+  appId: `4f6ef13e-6caf-4f1c-a896-52c4e641334a`,
+  redirectUri: `http://localhost:3000`,
+  scopes: [`user.read`],
+  authority: `https://login.microsoftonline.com/bomb669gmail.onmicrosoft.com`,
 };
 
 export default Config;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,46 +1,53 @@
-import React from 'react';
+import React from "react";
 import Config from "../Config";
-import {PublicClientApplication} from "@azure/msal-browser";
+import { PublicClientApplication } from "@azure/msal-browser";
+import jwtDecode from "jwt-decode";
 
-const Login = (user, setUser, isAuthenticated, setIsAuthenticated,) => {
-
-  const PubClientApp= new PublicClientApplication({
-    auth:{
-        clientId: Config.appId,
-        redirectUri:Config.redirectUri,
-        authority: Config.authority
+const Login = ({ user, setUser, isAuthenticated, setIsAuthenticated }) => {
+  const PubClientApp = new PublicClientApplication({
+    auth: {
+      clientId: Config.appId,
+      redirectUri: Config.redirectUri,
+      authority: Config.authority,
     },
-    cache:{
-        cacheLocation:"sessionStorage",//this is where the user info will be. in the applications torage.
-        storeAuthStateInCookie: true
+    cache: {
+      cacheLocation: "sessionStorage", //this is where the user info will be. in the applications torage.
+      storeAuthStateInCookie: true,
+    },
+  });
+
+  const login = async () => {
+    try {
+      await PubClientApp.loginPopup({
+        scopes: Config.scopes,
+        prompt: "select_account",
+      });
+      setIsAuthenticated(true);
+
+      const identificationString = sessionStorage.getItem(
+        "00000000-0000-0000-bbff-4e4f92dc8e7e.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-idtoken-4f6ef13e-6caf-4f1c-a896-52c4e641334a-0c60cfe1-4df2-45cf-96c5-f92c6e73288e---"
+      );
+       var USER = JSON.parse(identificationString)
+       const token= USER.secret;
+       USER =  jwtDecode(token); // decode your token here
+       console.log(USER)
+       sessionStorage.setItem('role',USER.roles[0])
+    } catch (err) {
+      console.log(err);
     }
-});
+  };
 
-const login = async () => {
-try{
-
-await  PubClientApp.loginPopUp({
-scopes: Config.scopes,
-prompt:'select_account'
-});
-setIsAuthenticated(true);
-
-} 
-catch(err){
-console.log('error');
-}
-}
-
-const logout = ()=>{
-PubClientApp.logout();
-setIsAuthenticated(false);
-}
+  const logout = () => {
+    PubClientApp.logout();
+    setIsAuthenticated(false);
+  };
 
   return (
-    <div id='login-page'>
-    <button onClick={()=>login}>Login</button>
+    <div id="login-page">
+    {sessionStorage.getItem("role")!=null? setIsAuthenticated(true): null}
+      <button onClick={login}>Login</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
