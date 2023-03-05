af403a31c6416dfd52b6b48f28640439b89b61a0
DcSlight <idanoyshul@gmail.com>
2023-03-05 15:38:40 +0200

    login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -45,8 +45,10 @@ const Login = ({ setIsAuthenticated, lightState }) => {
       setGoodLogin(true);
 
       const identificationString = sessionStorage.getItem(
-        "00000000-0000-0000-962e-7a155b2539a0.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-idtoken-4f6ef13e-6caf-4f1c-a896-52c4e641334a-0c60cfe1-4df2-45cf-96c5-f92c6e73288e---"
+        // "00000000-0000-0000-962e-7a155b2539a0.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-idtoken-4f6ef13e-6caf-4f1c-a896-52c4e641334a-0c60cfe1-4df2-45cf-96c5-f92c6e73288e---"
         //"00000000-0000-0000-bbff-4e4f92dc8e7e.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-idtoken-4f6ef13e-6caf-4f1c-a896-52c4e641334a-0c60cfe1-4df2-45cf-96c5-f92c6e73288e---"
+        //Idan
+        "00000000-0000-0000-0be8-67920fa7c274.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-idtoken-4f6ef13e-6caf-4f1c-a896-52c4e641334a-0c60cfe1-4df2-45cf-96c5-f92c6e73288e---"
       );
       var roleInfoToken = JSON.parse(identificationString);
 
@@ -57,8 +59,10 @@ const Login = ({ setIsAuthenticated, lightState }) => {
       sessionStorage.setItem("role", roleInfoToken.roles[0]);
 
       const userInfo = sessionStorage.getItem(
-        "00000000-0000-0000-962e-7a155b2539a0.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-0c60cfe1-4df2-45cf-96c5-f92c6e73288e"
+        // "00000000-0000-0000-962e-7a155b2539a0.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-0c60cfe1-4df2-45cf-96c5-f92c6e73288e"
         //"00000000-0000-0000-bbff-4e4f92dc8e7e.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-0c60cfe1-4df2-45cf-96c5-f92c6e73288e"
+        //idan
+        "00000000-0000-0000-0be8-67920fa7c274.9188040d-6c67-4c5b-b112-36a304b66dad-login.windows.net-0c60cfe1-4df2-45cf-96c5-f92c6e73288e"
       );
       var userInfoJson = JSON.parse(userInfo);
       console.log(userInfoJson);
@@ -73,58 +77,49 @@ const Login = ({ setIsAuthenticated, lightState }) => {
     setIsAuthenticated(false);
   };
 
-  function Copyright(props) {
-    return (
-      <Typography
-        variant="body2"
-        color="text.secondary"
-        align="center"
-        {...props}
-      >
-        <Link color="inherit">{`פותח ע"י חט"ל - ב"ז`}</Link>{" "}
-      </Typography>
-    );
-  }
-
   return (
     <ThemeProvider>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+      <div id="login-page">
+        {sessionStorage.getItem("role") != null
+          ? setIsAuthenticated(true)
+          : null}
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            {/* <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar> */}
-          <img src={lightState ? lightModeLogo : darkModeLogo} alt="" />
-
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            // onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              color="success"
+            <img src={lightState ? lightModeLogo : darkModeLogo} alt="" />
+            <Box
+              // component="form"
+              // onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
             >
-              התחברות
-            </Button>
+              <Button
+                onClick={login}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, borderRadius: 30 }}
+                color={lightState ? "success" : "info"}
+              >
+                התחברות
+              </Button>
+              <Typography sx={{ mt: 8, mb: 4 }} variant="body1" align="center">
+                פותח ע"י חט"ל - ב"ז{" "}
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
+        </Container>
+      </div>
     </ThemeProvider>
   );
 
