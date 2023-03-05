697c6cba4f60df6dc283a894caa925aa9912b745
DcSlight <idanoyshul@gmail.com>
2023-03-05 13:38:59 +0200

    darkmode and light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,51 +10,119 @@ import SelectSoldiers from "./pages/SelectSoldiers";
 import ChooseRole from "./pages/ChooseRole";
 import GeneralInput from "./pages/GeneralInput";
 
+import Image from "./image/background.png"; // Import using relative path
+//import mui
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+const themeLight = createTheme({
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "rgb(245,255,255)",
+          backgroundImage: `linear-gradient(71deg, rgba(245,255,255,1) 47%, rgba(173,225,229,1) 100%)`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+          height: "100%",
+        },
+      },
+    },
+  },
+});
+
+const themeDark = createTheme(
+  {
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          body: {
+            backgroundColor: "rgb(0,0,0)",
+            backgroundImage: `linear-gradient(71deg, rgba(0,0,0,1) 5%, rgba(33,48,125,1) 100%);`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
+            backgroundAttachment: "fixed",
+            height: "100%",
+          },
+        },
+      },
+    },
+    palette: {
+      text: {
+        primary: "#ffffff",
+      },
+    },
+  }
+
+  //   palette: {
+  //     background: {
+  //       default: "#222222",
+  //     },
+  //     text: {
+  //       primary: "#ffffff",
+  //     },
+  //   },
+  // }
+);
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [light, setLight] = useState(true);
 
   return (
-    <div className="App">
-      <BrowserRouter>
-        <img
+    <ThemeProvider theme={light ? themeLight : themeDark}>
+      <CssBaseline />
+      <Button onClick={() => setLight((prev) => !prev)}>Toggle Theme</Button>
+
+      <div className="App">
+        <BrowserRouter>
+          {/* <img
           className="demo-bg"
           src=" http://1.bp.blogspot.com/-BInHLTLYcSY/U9CdS4qEBoI/AAAAAAAAAac/Rt3ynw8GBy4/s1600/4.jpg"
           alt=""
-        />
-        <Routes>
-          {isAuthenticated ? (
+        /> */}
+          <Routes>
+            {isAuthenticated ? (
+              <Route
+                path="/"
+                element={<Home setIsAuthenticated={setIsAuthenticated} />}
+              />
+            ) : (
+              <Route
+                path="/"
+                element={<Login setIsAuthenticated={setIsAuthenticated} />}
+              />
+            )}
             <Route
-              path="/"
-              element={<Home setIsAuthenticated={setIsAuthenticated} />}
+              path="/SelectSoldiers/:rid/TestType/Questionary/:ttid"
+              element={<Questionary />}
             />
-          ) : (
             <Route
-              path="/"
-              element={<Login setIsAuthenticated={setIsAuthenticated} />}
+              path="/GeneralInput/ChooseRole/:rid/TestType/Questionary/:ttid"
+              element={<Questionary />}
             />
-          )}
-          <Route
-            path="/SelectSoldiers/:rid/TestType/Questionary/:ttid"
-            element={<Questionary />}
-          />
-          <Route
-            path="/GeneralInput/ChooseRole/:rid/TestType/Questionary/:ttid"
-            element={<Questionary />}
-          />
 
-          <Route path="/SelectSoldiers/:rid/TestType" element={<TestType />} />
+            <Route
+              path="/SelectSoldiers/:rid/TestType"
+              element={<TestType />}
+            />
 
-          <Route path="/SelectSoldiers/:rid" element={<SelectSoldiers />} />
-          <Route path="/MyTrainees" element={<SelectSoldiers />} />
-          <Route path="/MyTrainees/ChooseRole" element={<ChooseRole />} />
-          <Route path="/GeneralInput/ChooseRole" element={<ChooseRole />} />
-          <Route
-            path="/GeneralInput/ChooseRole/:rid/TestType"
-            element={<TestType />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
+            <Route path="/SelectSoldiers/:rid" element={<SelectSoldiers />} />
+            <Route path="/MyTrainees" element={<SelectSoldiers />} />
+            <Route path="/MyTrainees/ChooseRole" element={<ChooseRole />} />
+            <Route path="/GeneralInput/ChooseRole" element={<ChooseRole />} />
+            <Route
+              path="/GeneralInput/ChooseRole/:rid/TestType"
+              element={<TestType />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,22 @@ import { React, useEffect, useState } from "react";
 import Config from "../Config";
 import { PublicClientApplication } from "@azure/msal-browser";
 import jwtDecode from "jwt-decode";
-import "./Login.css";
+//import "./Login.css";
+
+//mui imports
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const Login = ({ setIsAuthenticated }) => {
   const [goodLogin, setGoodLogin] = useState(false);
@@ -54,13 +69,106 @@ const Login = ({ setIsAuthenticated }) => {
     setIsAuthenticated(false);
   };
 
+  function Copyright(props) {
+    return (
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        align="center"
+        {...props}
+      >
+        {"Copyright © "}
+        <Link color="inherit" href="https://mui.com/">
+          Your Website
+        </Link>{" "}
+        {new Date().getFullYear()}
+        {"."}
+      </Typography>
+    );
+  }
+
+  const theme = createTheme();
+
   return (
-    <div id="login-page">
-      {sessionStorage.getItem("role") != null ? setIsAuthenticated(true) : null}
-      <img id="star-logo" src="/star.jpg" alt="star Logo" />
-      <button onClick={login}>התחברות</button>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            // onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
+
+  // <div id="login-page">
+  //   {sessionStorage.getItem("role") != null ? setIsAuthenticated(true) : null}
+  //   <img id="star-logo" src="/star.jpg" alt="star Logo" />
+  //   <button onClick={login}>התחברות</button>
+  // </div>
 };
 
 export default Login;
